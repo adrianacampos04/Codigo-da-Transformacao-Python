68745b8f264da9b9ffdecdbcbc8252f983fe3743
Adriana Campos <105952624+adrianacampos04@users.noreply.github.com>
2025-04-17 12:17:18 -0300

    Add files via upload

---
+++ condicionais.py
@@ -0,0 +1,11 @@
+nota = 85
+if nota >= 90:
+ print("A")
+elif nota >= 80:
+ print("B")
+elif nota >= 70:
+ print("C")
+else:
+ print("Reprovado")
+
+# IMPORTANTE, SEMPRE UM ESPAÇO NA CAIXA DE  CONDIÇÃO SE NÃO DA ERRO
---
+++ logicos.py
@@ -0,0 +1,5 @@
+x = 10
+y = 5
+print(x > 5 and y < 10) # True
+print(x > 5 or y > 10) # True
+print(not(x > 5)) # False
